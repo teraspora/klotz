a101e722caf7e1308eaf27e5626e158ddb7c5c9c
John Lynch <johnlynch.32768@gmail.com>
2019-10-16 01:36:13 +0100

    Get gun moving and firing bullets.

--- scripts/main.js
+++ scripts/main.js
@@ -4,23 +4,29 @@
 
 
 
-let randInt = n => Math.floor(n * Math.random());
-let randCol = _ => {
-    let rgb = [randInt(100) + 155, randInt(100) + 155, randInt(100) + 155];
-    rgb[randInt(3)] /= 8;
-    let [r, g, b] = rgb;
-    return `rgb(${r}, ${g}, ${b})`;
-};
+const randInt = n => Math.floor(n * Math.random());
+// let randCol = _ => {
+//     let rgb = [randInt(100) + 155, randInt(100) + 155, randInt(100) + 155];
+//     rgb[randInt(3)] /= 8;
+//     let [r, g, b] = rgb;
+//     return `rgb(${r}, ${g}, ${b})`;
+// };
+
+const colours = ["#f5004a", "#2762ff", "#6359c0", "#36ff6a", "#044c3a", "#ffff00"];
+let bullets = [];
+
+const randCol = _ => colours[randInt(colours.length)];
 
 const c = document.getElementById(`field`);
 const [W, H] = [c.clientWidth, c.clientHeight];
-
+let bulletid = 0;
+let firing = false;
 
 // Need this class to keep track of which sliders are active
 class Ball {
     constructor(element, pos, vel, col) {
         this._element = element;
-        this._id = parseInt(element.id.slice(4))
+        this._id = +element.id.replace( /^\D+/g, '')
         this._pos = pos;
         this._vel = vel;
         this._col = col;
@@ -57,8 +63,16 @@ class Ball {
     set col(value) {
         this._col = value;
     }
-    
 }
+
+class Bullet extends Ball {
+    constructor(element, pos, vel, col) {
+        super(element, pos, vel, col);
+    }    
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
+
 let ball_count = 16;
 const ball_zero = document.getElementById(`ball-zero`);
 const main = document.querySelector(`main`);
@@ -106,8 +120,65 @@ function render() {
             }
         }
     });
-    
+
+    bullets.forEach(bullet => {
+        bullet.pos = {x: bullet.pos.x + bullet.vel.vx, y: bullet.pos.y - bullet.vel.vy};
+        if (bullet.pos.y < -500) {
+            bullet.element.remove();
+            bullets.splice(bullets.indexOf(bullet), 1);
+        }
+    });
     requestAnimationFrame(render);
 }
+
+// Create gun
+const gun = document.createElement(`div`);
+const gunwidth = 40;
+main.appendChild(gun);
+let gunpos = (W - gunwidth) / 2;
+gun.style.position = "relative";
+gun.style.width = `${gunwidth}px`;
+gun.style.height = `10px`;
+gun.style.backgroundColor  = `red`;
+gun.style.left = `${gunpos}px`;
+gun.style.top = `${gun.parentElement.offsetHeight - gun.offsetHeight - 40}px`;
+
+// ADD EVENT LISTENERS TO MOVE GUN ON [] input AND FIRE ON SPACE
+window.addEventListener('keydown', ev => {
+    key = ev.key || ev.keyCode;   // keyCode is an older standard
+    switch (key) {
+        case "[":
+            gunpos -= 20;
+            gun.style.left = `${gunpos}px`;   // transform =(translateX();
+            break;
+        case "]":
+            gunpos += 20;
+            gun.style.left = `${gunpos}px`;   // transform =(translateX();
+            break;
+        case " ":
+        firing = true;
+            const bulletwidth = 12;
+            const bulletpos = gunpos + gunwidth / 2 - bulletwidth / 2;
+            const new_bullet_element = document.createElement(`div`);
+            new_bullet_element.id = `bullet${bulletid++}`
+            const bullet = new Bullet(new_bullet_element, {x: bulletpos, y: 300}, {vx: 0, vy: 12}, 'green');
+            bullets.push(bullet);
+            new_bullet_element.style.position = "relative";
+            new_bullet_element.style.width = `${bulletwidth}px`;
+            new_bullet_element.style.height = `40px`;
+            new_bullet_element.style.backgroundColor  = bullet.col;
+            new_bullet_element.style.left = `${bulletpos}px`;
+            new_bullet_element.style.top = `${bullet.pos.y}px`; //  `${gun.style.top + 100}px`; // DEBUG STAT      `${bullet.parentElement.offsetHeight - bullet.offsetHeight - 40}px`;
+            new_bullet_element.style.zIndex = 255;
+            new_bullet_element.style.borderTopLeftRadius = `50%`;
+            new_bullet_element.style.borderTopRightRadius = `50%`;
+            main.appendChild(new_bullet_element);
+            break;
+        default:
+            ;
+            break;
+    }        
+});
+
 
 render();
