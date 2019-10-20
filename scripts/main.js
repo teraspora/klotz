b15adc28e36d2f12ffe7bf60f8516015d4789b94
John Lynch <johnlynch.32768@gmail.com>
2019-10-20 22:24:27 +0100

    Add scoreboard.

--- scripts/main.js
+++ scripts/main.js
@@ -2,20 +2,17 @@
 // Author:  John Lynch
 // October 2019
 
-
+// KEYS
+const gun_left_key = `ArrowLeft`;
+const gun_right_key = `ArrowRight`;
+const fire_bullet_key = ` `;
 
 const randInt = n => Math.floor(n * Math.random());
-// let randCol = _ => {
-//     let rgb = [randInt(100) + 155, randInt(100) + 155, randInt(100) + 155];
-//     rgb[randInt(3)] /= 8;
-//     let [r, g, b] = rgb;
-//     return `rgb(${r}, ${g}, ${b})`;
-// };
 
-const colours = ["#f5004a", "#2762ff", "#6359c0", "#36ff6a", "#044c3a", "#ffff00"];
 let bullets = [];
+let score = 0;
 
-const randCol = _ => colours[randInt(colours.length)];
+// const randCol = _ => colours[randInt(colours.length)];
 
 const c = document.getElementById(`field`);
 const [W, H] = [c.clientWidth, c.clientHeight];
@@ -24,13 +21,16 @@ let firing = false;
 
 // Need this class to keep track of which sliders are active
 class Ball {
-    constructor(element, pos, vel, col) {
+    #colours = ["#f5004a", "#2762ff", "#6359c0", "#36ff6a", "#044c3a", "#ffff00"];
+    #bullet_col = `#00c285`;
+    constructor(element, pos, vel, type) {
         this._element = element;
         this._id = +element.id.replace( /^\D+/g, '')
         this._pos = pos;
         this._vel = vel;
-        this._col = col;
-        this._element.style.backgroundColor = col;
+        this._type = type;
+        this._col = type != -1 ? this.#colours[type] : this.#bullet_col;
+        this._element.style.backgroundColor = this._col;
     }
     get element() {
         return this._element;
@@ -57,17 +57,27 @@ class Ball {
     set vel(value) {
         this._vel = value;
     }
+    get type() {
+        return this._type;
+    }
+    set type(value) {
+        this._type = value;
+        this._col = value != -1 ? this.#colours[value] : this.#bullet_col;
+    }
     get col() {
         return this._col;
     }
-    set col(value) {
-        this._col = value;
+    explode() {
+        score += 1000 * this.type;
+        document.getElementById(`score`).innerText = score;
+        this.element.remove();
+        balls.splice(balls.indexOf(this));
     }
 }
 
 class Bullet extends Ball {
-    constructor(element, pos, vel, col) {
-        super(element, pos, vel, col);
+    constructor(element, pos, vel) {
+        super(element, pos, vel, -1);
     }    
 }
 
@@ -80,7 +90,7 @@ const balls = Array(ball_count).fill().map((_, i) => {
     let new_ball_element = ball_zero.cloneNode(false);
     new_ball_element.id = `ball${i}`;
     main.appendChild(new_ball_element);
-    let new_ball = new Ball(new_ball_element, {x: randInt(W), y: randInt(H)}, {vx: randInt(11) + 1, vy: randInt(11) + 1}, randCol());
+    let new_ball = new Ball(new_ball_element, {x: randInt(W), y: randInt(H)}, {vx: randInt(11) + 1, vy: randInt(11) + 1}, randInt(6));
     return new_ball;
 });
 
@@ -95,6 +105,7 @@ let n = 0;
 function render() {
     for (let ball of balls) {
         const {vx, vy} = ball.vel;
+        // Bounce off a wall if necessary
         if (ball.pos.x >= W - ball.element.offsetWidth) {
             ball.vel.vx = -Math.abs(vx);
         }
@@ -107,8 +118,10 @@ function render() {
         if (ball.pos.y <= 0) {
             ball.vel.vy = Math.abs(vy);
         }
+        // Update ball position
         ball.pos = {x: ball.pos.x + ball.vel.vx, y: ball.pos.y + ball.vel.vy};
     }
+    // Test for balls colliding with other balls - not rigorous!
     [...balls].sort((b0, b1) => b0.pos.x == b1.pos.x ? b0.pos.y - b1.pos.y : b0.pos.x - b1.pos.x).forEach((ball, index, blist) => {
         if (index < blist.length - 1) {
             const other_ball = blist[index + 1];
@@ -120,12 +133,27 @@ function render() {
             }
         }
     });
-
+    // Update bullets
     bullets.forEach(bullet => {
         bullet.pos = {x: bullet.pos.x + bullet.vel.vx, y: bullet.pos.y - bullet.vel.vy};
         if (bullet.pos.y < -500) {
             bullet.element.remove();
             bullets.splice(bullets.indexOf(bullet), 1);
+        }
+        else {
+            balls.forEach(ball => {
+                // Test each ball to see if hit by bullet
+                if (Math.abs(ball.pos.x - bullet.pos.x) < ball.element.offsetWidth / 2 &&
+                    Math.abs(ball.pos.y - bullet.pos.y) < ball.element.offsetHeight / 2) {
+                        bullet.element.remove();
+                        bullets.splice(bullets.indexOf(bullet), 1);
+                        ball.explode();
+                        if (balls.length == 0) {
+                            alert(`You win with a score of ${score}!`);
+                            document.location.reload();
+                        }
+                }
+            });
         }
     });
     requestAnimationFrame(render);
@@ -147,16 +175,16 @@ gun.style.top = `${gun.parentElement.offsetHeight - gun.offsetHeight - 40}px`;
 window.addEventListener('keydown', ev => {
     key = ev.key || ev.keyCode;   // keyCode is an older standard
     switch (key) {
-        case "[":
+        case gun_left_key:
             gunpos -= 20;
             gun.style.left = `${gunpos}px`;   // transform =(translateX();
             break;
-        case "]":
+        case gun_right_key:
             gunpos += 20;
             gun.style.left = `${gunpos}px`;   // transform =(translateX();
             break;
-        case " ":
-        firing = true;
+        case fire_bullet_key:
+            firing = true;
             const bulletwidth = 12;
             const bulletpos = gunpos + gunwidth / 2 - bulletwidth / 2;
             const new_bullet_element = document.createElement(`div`);
